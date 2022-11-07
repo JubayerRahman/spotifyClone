c227f0cf45d4ea3c8679f3f38b949d887f87d1cb
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2022-11-08 01:25:12 +0600

    Update function.js

--- function.js
+++ function.js
@@ -1,9 +1,12 @@
 const MainPlayButton = document.getElementById("mainPlay");
 const gif = document.getElementById("gif");
 const prograssBar = document.getElementById("prograssBar");
+const mainSongName = document.getElementById("mainSongName");
+let index =0;
 const songitme = Array.from(document.getElementsByClassName("songitme"));
 let audioPlay = new Audio("./songs/1.mp3");
 //audioPlay.play();
+
 
 let songs = [
     {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg", time:"3:50"},
@@ -30,6 +33,7 @@ MainPlayButton.addEventListener("click", () => {
     MainPlayButton.classList.remove("fa-play");
     MainPlayButton.classList.add("fa-pause");
     gif.style.opacity = "1";
+    mainSongName.innerText = songs[index].songName;
   } else {
     audioPlay.pause();
     MainPlayButton.classList.remove("fa-pause");
@@ -46,4 +50,74 @@ audioPlay.addEventListener("timeupdate",()=>{
 prograssBar.addEventListener("change", ()=>{
     audioPlay.currentTime = prograssBar.value* audioPlay.duration/100;
 
+})
+
+const makePlay=()=>{
+  Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+    element.classList.remove("fa-circle-pause");
+    element.classList.add("fa-circle-play");
+  })
+}
+
+Array.from(document.getElementsByClassName("songItemPlay")).forEach((element)=>{
+  element.addEventListener("click",(e)=>{
+    if (audioPlay.paused || audioPlay.currentTime <= 0){
+    makePlay();
+    console.log(e.target);
+    index = parseInt(e.target.id);
+    e.target.classList.remove("fa-circle-play");
+    e.target.classList.add("fa-circle-pause");
+    audioPlay.src= `songs/${index+1}.mp3`;
+    audioPlay.currentTime =0;
+    audioPlay.play();
+    gif.style.opacity="1";
+    mainSongName.innerText = songs[index].songName;
+    MainPlayButton.classList.remove("fa-play");
+    MainPlayButton.classList.add("fa-pause");
+    }
+    else{
+      audioPlay.pause();
+      gif.style.opacity="0";
+      e.target.classList.add("fa-circle-play");
+      e.target.classList.remove("fa-circle-pause");
+      MainPlayButton.classList.remove("fa-pause");
+      MainPlayButton.classList.add("fa-play");
+    }
+  })
+})
+const next = document.getElementById("next").addEventListener("click", ()=>{
+  console.log("next")
+  if(index > 9){
+    index = 0;
+    document.getElementById("next").style.color="gray";
+    document.getElementById("nextDialog").innerHTML ="You have unloked the glitchin this system. Keep Calm The developer is looking for the solution."
+  }
+  else{
+    index +=1;
+      audioPlay.src= `songs/${index+1}.mp3`;
+      audioPlay.currentTime =0;
+      audioPlay.play();
+      gif.style.opacity="1";
+      mainSongName.innerText = songs[index].songName;
+      MainPlayButton.classList.remove("fa-play");
+      MainPlayButton.classList.add("fa-pause");
+  }
+})
+
+const previpus = document.getElementById("prvious").addEventListener("click", ()=>{
+  console.log("Previous")
+  if(index<=0){
+    index = 0;
+    document.getElementById("prvious").style.color="gray";
+  }
+  else{
+    index -=1;
+  }
+      audioPlay.src= `songs/${index+1}.mp3`;
+      audioPlay.currentTime =0;
+      audioPlay.play();
+      gif.style.opacity="1";
+      mainSongName.innerText = songs[index].songName;
+      MainPlayButton.classList.remove("fa-play");
+      MainPlayButton.classList.add("fa-pause");
 })
