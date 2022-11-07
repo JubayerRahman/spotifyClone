ed3a8f19ebc74c1ade0e51bee31c82c307224c66
JubayerRahman <68602999+JubayerRahman@users.noreply.github.com>
2022-11-07 10:02:07 +0600

    Update function.js

---
+++ function.js
@@ -0,0 +1,49 @@
+const MainPlayButton = document.getElementById("mainPlay");
+const gif = document.getElementById("gif");
+const prograssBar = document.getElementById("prograssBar");
+const songitme = Array.from(document.getElementsByClassName("songitme"));
+let audioPlay = new Audio("./songs/1.mp3");
+//audioPlay.play();
+
+let songs = [
+    {songName: "Warriyo - Mortals [NCS Release]", filePath: "songs/1.mp3", coverPath: "covers/1.jpg", time:"3:50"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg", time:"2:33"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg", time:"4:33"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg", time:"4:27"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg", time:"3:27"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg", time:"2:33"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg", time:"2:33"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg", time:"2:33"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg", time:"2:33"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg", time:"4:27"},
+]
+
+songitme.forEach((element, i)=>{
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+    element.getElementsByClassName("time")[0].innerText = songs[i].time;
+})
+
+MainPlayButton.addEventListener("click", () => {
+  if (audioPlay.paused || audioPlay.currentTime <= 0) {
+    audioPlay.play();
+    MainPlayButton.classList.remove("fa-play");
+    MainPlayButton.classList.add("fa-pause");
+    gif.style.opacity = "1";
+  } else {
+    audioPlay.pause();
+    MainPlayButton.classList.remove("fa-pause");
+    MainPlayButton.classList.add("fa-play");
+    gif.style.opacity = "0";
+  }
+});
+
+audioPlay.addEventListener("timeupdate",()=>{
+    let progerss = parseInt((audioPlay.currentTime/audioPlay.duration)*100);
+    prograssBar.value= progerss;
+})
+
+prograssBar.addEventListener("change", ()=>{
+    audioPlay.currentTime = prograssBar.value* audioPlay.duration/100;
+
+})
